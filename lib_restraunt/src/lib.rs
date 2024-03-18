d2973c0c0c3f953592993f1d5cf52f3aa5bce2a0
laborless <wyj1023@gmail.com>
2024-03-18 23:00:10 +0100

    7_manage_project

---
+++ lib_restraunt/src/lib.rs
@@ -0,0 +1,140 @@
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+}
+
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//     }
+// }
+
+// pub use crate::front_of_house::hosting;
+
+// pub fn eat_at_restaurant() {
+//     hosting::add_to_waitlist();
+// }
+
+// use std::fmt;
+// use std::io;
+
+// fn function1() -> fmt::Result {
+//     // --snip--
+// }
+
+// fn function2() -> io::Result<()> {
+//     // --snip--
+// }
+
+// use std::collections::HashMap;
+
+// fn main() {
+//     let mut map = HashMap::new();
+//     map.insert(1, 2);
+// }
+
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//     }
+// }
+
+// use crate::front_of_house::hosting::add_to_waitlist;
+
+// pub fn eat_at_restaurant() {
+//     add_to_waitlist();
+// }
+
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//     }
+// }
+
+// use crate::front_of_house::hosting;
+
+// mod customer {
+//     pub fn eat_at_restaurant() {
+//         super::hosting::add_to_waitlist();
+//     }
+// }
+
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//     }
+// }
+
+// use crate::front_of_house::hosting;
+
+// pub fn eat_at_restaurant() {
+//     hosting::add_to_waitlist();
+// }
+
+// mod back_of_house {
+//     pub enum Appetizer {
+//         Soup,
+//         Salad,
+//     }
+// }
+
+// pub fn eat_at_restaurant() {
+//     let order1 = back_of_house::Appetizer::Soup;
+//     let order2 = back_of_house::Appetizer::Salad;
+// }
+
+// mod back_of_house {
+//     pub struct Breakfast {
+//         pub toast: String,
+//         seasonal_fruit: String,
+//     }
+
+//     impl Breakfast {
+//         pub fn summer(toast: &str) -> Breakfast {
+//             Breakfast {
+//                 toast: String::from(toast),
+//                 seasonal_fruit: String::from("peaches"),
+//             }
+//         }
+//     }
+// }
+
+// pub fn eat_at_restaurant() {
+//     // Order a breakfast in the summer with Rye toast
+//     let mut meal = back_of_house::Breakfast::summer("Rye");
+//     // Change our mind about what bread we'd like
+//     meal.toast = String::from("Wheat");
+//     println!("I'd like {} toast please", meal.toast);
+
+//     // The next line won't compile if we uncomment it; we're not allowed
+//     // to see or modify the seasonal fruit that comes with the meal
+//     // meal.seasonal_fruit = String::from("blueberries");
+// }
+
+// fn deliver_order() {}
+
+// mod back_of_house {
+//     fn fix_incorrect_order() {
+//         cook_order();
+//         super::deliver_order();
+//     }
+
+//     fn cook_order() {}
+// }
+
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//     }
+// }
+
+// pub fn eat_at_restaurant() {
+//     // Absolute path
+//     crate::front_of_house::hosting::add_to_waitlist();
+
+//     // Relative path
+//     front_of_house::hosting::add_to_waitlist();
+// }
